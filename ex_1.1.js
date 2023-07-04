67d5cec760bc041da7ee6f6db2c68ede28cac17f
AHKH1990 <135972116+AHKH1990@users.noreply.github.com>
2023-07-04 20:58:44 +0300

    ex_1.1

---
+++ ex_1.1.js
@@ -0,0 +1,16 @@
+let message = "You enter a dimly lit room.";
+let treasureCount = 33;
+let key = {
+    type: "golden key",
+    };
+let isTrapActive = false
+let secretPassage = 0
+let mysteriousPassage;
+let ancientSymbol = Symbol("ancient symbol")
+console.log(message);
+console.log(treasureCount);
+console.log(key);
+console.log(isTrapActive);
+console.log(secretPassage);
+console.log(mysteriousPassage);
+console.log(ancientSymbol);
